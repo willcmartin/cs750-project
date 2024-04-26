b590e23f4fbf41f20719a270244301f64a8c852f
Will Martin <wcmartin01@gmail.com>
2024-04-26 16:40:51 -0500

    fixed data

--- detection/classifier.py
+++ detection/classifier.py
@@ -28,11 +28,12 @@ targetC = np.full(dataC.shape[0], 2)
 
 all_data = np.concatenate((dataA, dataB, dataC), axis=0)
 all_target = np.concatenate((targetA, targetB, targetC), axis=0)
-  
+
 # Convert the data to PyTorch tensors 
 X = torch.tensor(all_data, dtype=torch.float32) 
 y = torch.tensor(all_target, dtype=torch.long) 
 
+# torch.set_printoptions(threshold=10_000)
 print(X)
 print(y)
   
@@ -43,18 +44,18 @@ X = (X - mean) / std
   
 # Split the dataset into training and validation sets 
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42) 
-  
+
 # Create PyTorch Datasets 
 train_dataset = TensorDataset(X_train, y_train) 
 val_dataset = TensorDataset(X_val, y_val) 
   
 # Define the data loaders 
-batch_size = 16
+batch_size = 32
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True) 
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
 # Define the model 
-model = LogisticRegression(input_size=16, num_classes=3) 
+model = LogisticRegression(input_size=14, num_classes=3) 
   
 # Check for cuda 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') 
